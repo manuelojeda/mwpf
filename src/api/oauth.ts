7879463b94e07b374c2f493f263e2d6b8fec3074
Manuel Ojeda <manuelojedasistemas@gmail.com>
2020-03-27 20:28:09 -0700

    Added TypeScript

---
+++ src/api/oauth.ts
@@ -0,0 +1,4 @@
+// import { post } from 'axios'
+
+const clientId: string = ''
+const clientSecret: string = ''
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import CompApi from '@vue/composition-api'
+import BootstrapVue from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// CSS global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(CompApi)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
 new Vue({
   router,
